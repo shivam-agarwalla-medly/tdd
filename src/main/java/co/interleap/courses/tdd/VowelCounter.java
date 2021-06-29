3d1677ff3a5b7e5aff396b48990da7a7ce9b447a
Shivam <shivam.agarwalla@medly.com>
2021-06-29 15:16:39 +0530

    Third Step

--- src/main/java/co/interleap/courses/tdd/VowelCounter.java
+++ src/main/java/co/interleap/courses/tdd/VowelCounter.java
@@ -1,9 +1,12 @@
 package co.interleap.courses.tdd;
 
+import java.util.Locale;
+
 public class VowelCounter {
     public int vowelCount(String string)
     {
         int counter=0;
+        string=string.toLowerCase();
 
         for(int i=0;i<string.length();i++)
         {
